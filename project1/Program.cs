3185d441ca171b00a5d5420185470fe8a99e67c2
neo-dev <sei133dirsoa@gmail.com>
2026-09-24 23:38:23 -0300

    add few more projects on training room

---
+++ project1/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Hello! Please write a number: ");
+            string? str = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(str))
+            {
+                Console.WriteLine("Input cannot be empty. Please enter a valid number.");
+                str = Console.ReadLine();
+            }
+            try
+            {
+                var fa = int.Parse(str ?? "0");
+
+                if (fa > 0)
+                {
+                    Console.WriteLine("Positive number: " + fa);
+                }
+                else if (fa < 0)
+                {
+                    Console.WriteLine("Negative number: " + fa);
+                }
+                else
+                {
+                    Console.WriteLine("Zero: " + fa);
+                }
+            }
+
+
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                continue;
+            }
+        }
+    }
+}
